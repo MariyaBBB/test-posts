f0e5ea3c1326728067542772942681bfd398530c
Mariya Moiseeva <mash.dobarina@gmail.com>
2023-07-07 23:58:01 +0300

    Настроила загрузку данных через useEffect

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,17 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
 import { Spinner } from "react-bootstrap";
 import { useGetPostsQuery } from "../store/posts.api";
 import Pages from "./Pages";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "../store/slice";
 
 export default function Posts() {
-    const { data, isLoading, error } = useGetPostsQuery();
+    const posts = useSelector((state) => state.posts);
+    const { data: fetchedData, isLoading } = useGetPostsQuery();
+    const dispatch = useDispatch();
+   
+    useEffect(() => {
+        if (fetchedData) {
+            dispatch(setPosts(fetchedData));
+        }
+    }, [fetchedData, dispatch]);
+
     const [currentPage, setCurrentPage] = useState(1);
     const [postsPerPage] = useState(10);
-
     const lastPostIndex = currentPage * postsPerPage;
     const firstPostIndex = lastPostIndex - postsPerPage;
-    const currentPosts = data ? data.slice(firstPostIndex, lastPostIndex): 'error';
+    const currentPosts = posts ? posts.slice(firstPostIndex, lastPostIndex): 'error';
     const paginate = (pageNumber) => setCurrentPage(pageNumber)
   
     return(
@@ -23,16 +33,16 @@ export default function Posts() {
                 id={post.id}
                 title={post.title}
                 body={post.body}
+                userId={post.userId}
             ></Post>
           ))}
-               {data ? 
+               {posts ? 
                         <Pages 
                             postsPerPage={postsPerPage} 
-                            totalPosts={data.length}
+                            totalPosts={posts.length}
                             paginate={paginate}
                         /> : <></>
                }
-
         </>
     )
 }
