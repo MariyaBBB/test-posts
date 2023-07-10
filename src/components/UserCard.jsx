973d5a8871bf44a3da996bce8bbc2cef72b72030
Mariya Moiseeva <mash.dobarina@gmail.com>
2023-07-10 15:42:21 +0300

    Изменила представление карточки пользователя

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useGetUserDataQuery } from "../store/posts.api";
-import { Spinner, Card, Image } from "react-bootstrap";
+import { Spinner, Accordion } from "react-bootstrap";
 
 
 export default function UserCard({userId}) {
@@ -9,18 +9,19 @@ export default function UserCard({userId}) {
         <>
             {isLoading ? <Spinner /> : 
              <div>
-                <Card border="dark" style={{ width: '30rem'}} className="mb-2 me-2 ms-2 w-400">
-                    <Card.Body>
-                        <Card.Title>
-                            { data.name }, username {data.username}
-                        </Card.Title>
-                        <Card.Text>
+                <Accordion className="me-2 ms-2 mb-4 mt-2">
+                    <Accordion.Item>
+                        <Accordion.Header>
+                            Information about { data.name }
+                        </Accordion.Header>
+                        <Accordion.Body>
+                            <b>Username: </b> {data.username} <br/>
                             <b>Email: </b>{ data.email } <br/>
-                            <b>Адрес: </b>{ data.address.city} {data.address.street} {data.address.suite} <br/>
+                            <b>Adress: </b>{ data.address.city} {data.address.street} {data.address.suite} <br/>
                             <b>Tel.</b> {data.phone}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
              </div>
             }
         </>
